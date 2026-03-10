75d0c36404b64471493293fff947c5275c7164af
spertuzz <141872211+spertuzz@users.noreply.github.com>
2026-03-09 21:44:45 -0500

    Add files via upload

---
+++ phys.js
@@ -0,0 +1,439 @@
+// Global parameters
+var rbs = []
+var timescale = 1
+var g = 10
+
+// Templates
+let ball_template = {
+	type: 'Ball',
+	radius: 1
+}
+let polygon_template = {
+	type: 'Polygon',
+	vertices: [  // Relative to the center
+		new Vector2(1, 1),
+		new Vector2(1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, 1)
+	]
+}
+
+// Utility Formulas
+
+// Area of a polygon by applying the shoelace formula
+function polyArea(vertices, signed=false) {
+	let a = 0
+	let len = vertices.length;
+	for (let i = 0; i < len; i++) {
+		let v1 = vertices[i]
+		let v2 = vertices[(i + 1 + len) % len]
+		a += (v1.y + v2.y) * (v1.x - v2.x) / 2
+	}
+	return signed ? a : Math.abs(a)
+}
+
+// Overlap size between two projections
+function overlap(a, b) {
+	if (a[0] >= b[1] || b[0] >= a[1]) return 0
+	return Math.min(a[1], b[1]) - Math.max(a[0], b[0])
+}
+
+// 2D vector class
+
+class Vector2 {
+	
+	constructor(x=0, y=0) {
+		this.x = x
+		this.y = y
+	}
+	
+	// Arithmetic operations
+	
+	add(that) {
+		return new Vector2(this.x + that.x, this.y + that.y)
+	}
+	
+	subtract(that) {
+		return new Vector2(this.x - that.x, this.y - that.y)
+	}
+	
+	multiply(scalar) {
+		return new Vector2(this.x * scalar, this.y * scalar)
+	}
+	
+	divide(scalar) {
+		if (scalar === 0) return new Vector2()
+		return new Vector2(this.x / scalar, this.y / scalar)
+	}
+	
+	// Products
+	
+	dot(that) {
+		return this.x * that.x + this.y * that.y
+	}
+	
+	cross(that) {
+		return this.x * that.y - this.y * that.x
+	}
+	
+	// Angle
+	
+	theta() {
+		return Math.atan2(this.y, this.x)
+	}
+	
+	// Scale operations
+	
+	magnitude() {
+		return Math.sqrt(this.x ** 2 + this.y ** 2)
+	}
+	
+	normalize() {
+		const mag = this.magnitude()
+		if (mag === 0) return new Vector2()
+		return new Vector2(this.x / mag, this.y / mag)
+	}
+	
+	// Perpendicular vector
+	
+	perp() {
+		return new Vector2(-this.y, this.x)
+	}
+}
+
+// Rigidbody class (physics object)
+
+class Rigidbody {
+	
+	constructor(mass=0, pos=null, shape={type: 'Ball', radius: 1}, bounce=0.5, vel=null) {
+		this.mass = mass
+		this.pos = pos || new Vector2()
+		this.vel = vel || new Vector2()
+		this.force = new Vector2()
+		this.shape = shape
+		this.bounce = bounce
+		this.shape.centroid = this.centroid()
+		this.triangulate()
+		rbs.push(this)
+	}
+	
+	// Update positions each frame
+	
+	update(dt=0) {
+		if (this.mass === 0) return
+		let g_vector = new Vector2(0, -g)
+		this.force = this.force.add(g_vector)
+		
+		let f_impulse = this.force.multiply(dt)
+		this.addImpulse(f_impulse)
+		this.pos = this.pos.add(this.vel.multiply(dt))
+		
+		this.force = new Vector2()
+	}
+	
+	// Adds an impulse to the object
+	
+	addImpulse(impulse=null) {
+		if (this.mass === 0) return
+		impulse = impulse || new Vector2()
+		this.vel = this.vel.add(impulse.divide(this.mass))
+	}
+	
+	// Calculates the center of mass
+	
+	centroid() {
+		if (this.shape.type == 'Ball') {
+			return new Vector2(this.pos.x, this.pos.y)
+		}
+		else if (this.shape.type == 'Polygon') {
+			let xSum = 0
+			let ySum = 0
+			let len = this.shape.vertices.length
+			for (let i = 0; i < len; i++) {
+				let v1 = this.shape.vertices[i]
+				let v2 = this.shape.vertices[(i + 1 + len) % len]
+				xSum += (v1.x + v2.x) * v1.cross(v2)
+				ySum += (v1.y + v2.y) * v1.cross(v2)
+			}
+			let A = polyArea(this.shape.vertices, true)
+			xSum /= 6 * A
+			ySum /= 6 * A
+			return new Vector2(xSum, ySum)
+		}
+	}
+	
+	// Triangulates the rigidbody for collision calculations
+	
+	triangulate() {
+		if (this.shape.type != 'Polygon') return false
+		let triangles = []
+		let purgatory = []
+		for (let i = 0; i < this.shape.vertices.length; i++) {
+			let vertex = this.shape.vertices[i]
+			purgatory.push(new Vector2(vertex.x, vertex.y))
+		}
+		while (purgatory.length > 3) {
+			// Ear finding phase
+			let len = purgatory.length;
+			let toRemove = null
+			for (let i = 0; i < len; i++) {
+				// Gather 3 consecutive vertices
+				let a = purgatory[(i - 1 + len) % len]
+				let b = purgatory[i]
+				let c = purgatory[(i + 1 + len) % len]
+				
+				// Create vectors pointing from middle vertex to the outer ones
+				let va = a.subtract(b)
+				let vc = c.subtract(b)
+				
+				// Use cross product formula to determine if the angle is convex
+				let cross = va.cross(vc)
+				if (cross > 0) {
+					let ear = true
+					// Checking phase (if there are other vertices inside)
+					let goal = polyArea([a, b, c])
+					for (let j = 0; j < len; j++) {
+						let v = purgatory[j]
+						if (v == a || v == b || v == c) continue
+						let a1 = polyArea([v, b, c])
+						let a2 = polyArea([a, v, c])
+						let a3 = polyArea([a, b, v])
+						if (a1 + a2 + a3 <= goal + 0.001) {
+							ear = false
+							break
+						}
+					}
+					if (ear) {
+						toRemove = i
+						triangles.push([a, b, c])
+						break
+					}
+				}
+			}
+			if (toRemove != null) {
+				purgatory.splice(toRemove, 1)
+			}
+			else {
+				purgatory = null
+				break
+			}
+		}
+		if (purgatory == null) {
+			return false
+		}
+		triangles.push(purgatory)
+		this.shape.triangles = triangles
+		return true
+	}
+	
+	// Projects the rigidbody's vertices onto a normalized axis for the SAT
+	
+	project(ax, vertices=null) {
+		// Normalize axis for dot product calculations
+		ax = ax.normalize()
+		if (this.shape.type == 'Ball') {
+			// Set target points as those opposite on the circle on the axis
+			let v1 = this.pos.add(ax.multiply(this.shape.radius))
+			let v2 = this.pos.subtract(ax.multiply(this.shape.radius))
+			// Get dot products
+			let dot1 = v1.dot(ax)
+			let dot2 = v2.dot(ax)
+			return [Math.min(dot1, dot2), Math.max(dot1, dot2)]
+		}
+		else if (this.shape.type == 'Polygon') {
+			let target = vertices || this.shape.vertices
+			let min = Infinity
+			let max = -Infinity
+			for (let i = 0; i < target.length; i++) {
+				// Calculate projection for each vertex
+				let v = this.pos.add(target[i])
+				let dot = v.dot(ax)
+				if (dot < min) {
+					min = dot
+				}
+				if (dot > max) {
+					max = dot
+				}
+			}
+			return [min, max]
+		}
+	}
+	
+	// Gets the perpendicular axes to project onto
+	
+	getPerpAxes(vertices=null) {
+		if (this.shape.type != 'Polygon') return false
+		let target = vertices || this.shape.vertices
+		let axes = []
+		for (let i = 0; i < target.length; i++) {
+			let v1 = target[i]
+			let v2 = target[(i + 1 + target.length) % target.length]
+			let edge = v2.subtract(v1)
+			axes.push(edge.perp().normalize())
+		}
+		return axes
+	}
+	
+}
+
+// Detects collisions and returns the normal and overlap depth
+
+function detectCollision(a, b) {
+	let a_ball = a.shape.type == 'Ball'
+	let b_ball = b.shape.type == 'Ball'
+	
+	// Ball-on-ball collision
+	if (a_ball && b_ball) {
+		// Calculate sum of radiuses, being, the max distance where they don't touch
+		let combined_rad = a.shape.radius + b.shape.radius
+		let pos_diff = b.pos.subtract(a.pos)
+		let mag = pos_diff.magnitude()
+		
+		// Determine if the distance between them is less than this max
+		if (mag < combined_rad) {
+			return {
+				normal: pos_diff.normalize(),
+				depth: combined_rad - mag
+			}
+		}
+		
+		return false
+	}
+	
+	// Polygon-on-ball collision
+	else if (a_ball || b_ball) {
+		let ball = a_ball ? a : b
+		let poly = a_ball ? b : a
+		let triangles = poly.shape.triangles
+		for (let i = 0; i < triangles.length; i++) {
+			let triangle = triangles[i]
+			let axes = poly.getPerpAxes(triangle)
+			let closestVertex = triangle[0]
+			let minDist = closestVertex.subtract(ball.pos).magnitude()
+			for (let j = 1; j < triangle.length; j++) {
+				let v = triangle[j]
+				let dist = v.subtract(ball.pos).magnitude()
+				if (dist < minDist) {
+					closestVertex = v
+					minDist = dist
+				}
+			}
+			let pointAx = ball.pos.subtract(closestVertex)
+		}
+	}
+	
+	// Polygon-on-polygon collision
+	else {
+		// Gather axis for SAT comparison
+		let a_triangles = a.shape.triangles
+		let b_triangles = b.shape.triangles
+		for (let i = 0; i < a_triangles.length; i++) {
+			for (let j = 0; j < b_triangles.length; j++) {
+				let min = Infinity
+				let bestAx = null
+				let axes = []
+				axes.push(...a.getPerpAxes(a_triangles[i]))
+				axes.push(...b.getPerpAxes(b_triangles[j]))
+				for (let x = 0; x < axes.length; x++) {
+					let ax = axes[x]
+					let t1 = a_triangles[i]
+					let t2 = b_triangles[j]
+					let p1 = a.project(ax, t1)
+					let p2 = b.project(ax, t2)
+					let depth = overlap(p1, p2)
+					if (depth === 0) {
+						bestAx = null
+						break
+					}
+					else if (Math.abs(depth) < min) {
+						min = Math.abs(depth)
+						bestAx = ax
+					}
+				}
+				if (bestAx != null) {
+					let center = b.pos.subtract(a.pos)
+					if (bestAx.dot(center) < 0) {
+						bestAx = bestAx.multiply(-1)
+					}
+					return {
+						normal: bestAx,
+						depth: min
+					}
+				}
+			}
+		}
+	}
+}
+
+// Corrects the overlap caused by collisions
+
+function correctCollision(a, b, info) {
+	// Gather inverse masses for later calculations
+	let a_inv = a.mass === 0 ? 0 : 1 / a.mass
+	let b_inv = b.mass === 0 ? 0 : 1 / b.mass
+	let total_inv = a_inv + b_inv
+	
+	// Parameters for collision correction
+	let slop = 0.01
+	let frac = 0.25
+	
+	// Calculate vector to separate objects
+	let mag = frac * Math.max(info.depth - slop, 0) / total_inv
+	let vector = info.normal.multiply(mag)
+	
+	// Add relevant vectors scaled by inverse mass
+	a.pos = a.pos.subtract(vector.multiply(a_inv))
+	b.pos = b.pos.add(vector.multiply(b_inv))
+}
+
+// Resolves collisions by adding impulses
+
+function resolveCollision(a, b, info) {
+	// Gather inverse masses for later
+	let a_inv = a.mass === 0 ? 0 : 1 / a.mass
+	let b_inv = b.mass === 0 ? 0 : 1 / b.mass
+	let total_inv = a_inv + b_inv
+	
+	// Ignore collisions between two immovable masses
+	if (total_inv === 0) return
+	
+	// Dot product to later scale for collision impulse
+	let vel_diff = b.vel.subtract(a.vel)
+	let restitution = a.bounce * b.bounce
+	let impulse = vel_diff.dot(info.normal)
+	
+	// Ignore objects already moving away from each other
+	if (impulse > 0) return
+	
+	// Finalize impulse magnitute calculation
+	impulse *= -(1 + restitution)
+	impulse /= total_inv
+	
+	// Add relevant impulses
+	a.addImpulse(info.normal.multiply(-impulse))
+	b.addImpulse(info.normal.multiply(impulse))
+}
+
+// Calculate one physics step
+
+function step(dt) {
+	// Update phase
+	for (let i = 0; i < rbs.length; i++) {
+		rbs[i].update(dt * timescale)
+	}
+	
+	// Collision detection phase (loop through all pairs of rigidbodies)
+	for (let i = 0; i < rbs.length; i++) {
+		for (let j = i + 1; j < rbs.length; j++) {
+			let a = rbs[i]
+			let b = rbs[j]
+			let info = detectCollision(a, b)
+			
+			// If collision is valid
+			if (info) {
+				correctCollision(a, b, info)
+				resolveCollision(a, b, info)
+			}
+		}
+	}
+}
