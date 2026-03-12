b3db38deeea6cd1cb7501c632fc3ae28cfbdd083
spertuzz <141872211+spertuzz@users.noreply.github.com>
2026-03-12 14:42:17 -0500

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -1,1 +1,18 @@
+// Rigidbodies
 
+// Immovable floor
+new Rigidbody(0, new Vector2(0, 10), 0, {
+    type: 'Polygon',
+    vertices: [
+        new Vector2(200, 10),
+        new Vector2(200, -10),
+        new Vector2(-200, -10),
+        new Vector2(-200, 10)
+    ]
+}, 1)
+
+// Bouncy ball
+new Rigidbody(10, new Vector2(0, 200), 0, {
+    type: 'Ball',
+    radius: 10
+}, 1)
