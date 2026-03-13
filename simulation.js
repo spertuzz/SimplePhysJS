f069f8a62258933fe4838dff94c79758030a9c0a
spertuzz <141872211+spertuzz@users.noreply.github.com>
2026-03-12 20:08:06 -0500

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -1,18 +1,80 @@
 // Rigidbodies
 
-// Immovable floor
+// Immovable walls
+
 new Rigidbody(0, new Vector2(0, 10), 0, {
     type: 'Polygon',
     vertices: [
-        new Vector2(200, 10),
-        new Vector2(200, -10),
-        new Vector2(-200, -10),
-        new Vector2(-200, 10)
+        new Vector2(300, 10),
+        new Vector2(300, -10),
+        new Vector2(-300, -10),
+        new Vector2(-300, 10)
     ]
 }, 1)
 
-// Bouncy ball
+new Rigidbody(0, new Vector2(0, 440), 0, {
+    type: 'Polygon',
+    vertices: [
+        new Vector2(300, 10),
+        new Vector2(300, -10),
+        new Vector2(-300, -10),
+        new Vector2(-300, 10)
+    ]
+}, 1)
+
+new Rigidbody(0, new Vector2(-290, 225), 0, {
+    type: 'Polygon',
+    vertices: [
+        new Vector2(10, 205),
+        new Vector2(10, -205),
+        new Vector2(-10, -205),
+        new Vector2(-10, 205)
+    ]
+}, 1)
+
+new Rigidbody(0, new Vector2(290, 225), 0, {
+    type: 'Polygon',
+    vertices: [
+        new Vector2(10, 205),
+        new Vector2(10, -205),
+        new Vector2(-10, -205),
+        new Vector2(-10, 205)
+    ]
+}, 1)
+
+// Moving objects
+
 new Rigidbody(10, new Vector2(0, 200), 0, {
     type: 'Ball',
     radius: 10
+}, 1)
+
+new Rigidbody(10, new Vector2(100, 200), 0, {
+    type: 'Polygon',
+    vertices: [
+        new Vector2(20, 20),
+        new Vector2(20, -20),
+        new Vector2(-20, -20),
+        new Vector2(-20, 20)
+    ]
+}, 1, /*vel=new Vector2(100, 200)*/)
+
+
+new Rigidbody(10, new Vector2(140, 100), 0, {
+    type: 'Ball',
+    radius: 10
+}, 1, vel=new Vector2(10, 10))
+
+new Rigidbody(20, new Vector2(-100, 50), 0, {
+    type: 'Ball',
+    radius: 20
+}, 1, vel=new Vector2(20, 20))
+
+new Rigidbody(10, new Vector2(-120, 150), 20, {
+    type: 'Polygon',
+    vertices: [
+        new Vector2(20, 20),
+        new Vector2(20, -20),
+        new Vector2(-20, -20),
+    ]
 }, 1)
